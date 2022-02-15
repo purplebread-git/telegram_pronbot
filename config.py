10c6e78f33a11e722300672d8fc073bfc55a7128
purplebread888 <mnogotochiebrmethho@mail.ru>
2022-02-16 01:19:21 +0300

    Fix of decouple work

--- config.py
+++ config.py
@@ -1,17 +1,17 @@
-from decouple import config
+from decouple import config, Csv
 
 # for bot.py
 
-BOT_TOKEN = config('BOT_TOKEN', default='')  # Токен бота
+BOT_TOKEN = config('BOT_TOKEN')  # Токен бота
 
-admin_id = config('admin_id', default='')  # Твой айди аккаунта
+admin_id = config('admin_id', cast=int)  # Твой айди аккаунта
 
-kanals = config('kanals', default='')  # Список айди каналов, по которым бот будет рассылать контент
+kanals = config('kanals', cast=Csv(int))  # Список айди каналов, по которым бот будет рассылать контент
 
 # for grabber.py
 
-api_id = config('api_id', default='')  # Id аккаунта
-api_hash = config('api_hash', default='')  # Hash аккаунта
+api_id = config('api_id', cast=int)  # Id аккаунта
+api_hash = config('api_hash')  # Hash аккаунта
 
-my_channel_id = config('my_channel_id', default='')  # Айди бота, в который grabber будет пересылать контент
-channels = config('channels', default='')  # Список каналов с которых grabber будет парсить контент
+my_channel_id = config('my_channel_id', cast=int)  # Айди бота, в который grabber будет пересылать контент
+channels = config('channels', cast=Csv(int))  # Список каналов с которых grabber будет парсить контент
